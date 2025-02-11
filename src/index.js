260a3c65222ee9b5a4b9a73070c8d1807136c348
Dylan <57853417+4812571@users.noreply.github.com>
2025-02-11 17:00:26 -0600

    feat: relay cctp events

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { Server } from 'socket.io';
 import http from 'http';
 
-async function onEvent(eventName, eventData, io) {
+async function dispatchEvent(eventName, eventData, io) {
     console.log(`Server received event: ${eventName}`, eventData);
     io.emit(eventName, eventData);
 }
@@ -11,17 +11,34 @@ function onInsert(tableName) {
     return { event: 'INSERT', schema: 'public', table: tableName };
 }
 
+function onUpdate(tableName) {
+    return { event: 'UPDATE', schema: 'public', table: tableName };
+}
+
 async function main() {
     const { SUPABASE_DATA_URL, SUPABASE_DATA_KEY } = process.env;
 
     if (!SUPABASE_DATA_URL) {
         throw new Error('Missing SUPABASE_DATA_URL environment variable');
     }
+
     if (!SUPABASE_DATA_KEY) {
         throw new Error('Missing SUPABASE_DATA_KEY environment variable');
     }
 
     const supabaseData = createClient(SUPABASE_DATA_URL, SUPABASE_DATA_KEY);
+    
+    const { SUPABASE_APP_URL, SUPABASE_APP_KEY } = process.env;
+
+    if (!SUPABASE_APP_URL) {
+        throw new Error('Missing SUPABASE_APP_URL environment variable');
+    }
+
+    if (!SUPABASE_APP_KEY) {
+        throw new Error('Missing SUPABASE_APP_KEY environment variable');
+    }
+
+    const supabaseApp = createClient(SUPABASE_APP_URL, SUPABASE_APP_KEY);
 
     const server = http.createServer();
     const io = new Server(server, {
@@ -37,10 +54,16 @@ async function main() {
 
     supabaseData
         .channel('realtime')
-        .on('postgres_changes', onInsert('order_placed'), payload => onEvent('order_placed', payload.new, io))
-        .on('postgres_changes', onInsert('match_created'), payload => onEvent('match_created', payload.new, io))
-        .on('postgres_changes', onInsert('match_executed'), payload => onEvent('match_executed', payload.new, io))
-        .on('postgres_changes', onInsert('swap_filled'), payload => onEvent('swap_filled', payload.new, io))
+        .on('postgres_changes', onInsert('order_placed'), payload => dispatchEvent('order_placed', payload.new, io))
+        .on('postgres_changes', onInsert('match_created'), payload => dispatchEvent('match_created', payload.new, io))
+        .on('postgres_changes', onInsert('match_executed'), payload => dispatchEvent('match_executed', payload.new, io))
+        .on('postgres_changes', onInsert('swap_filled'), payload => dispatchEvent('swap_filled', payload.new, io))
+        .subscribe();
+
+    supabaseApp
+        .channel('realtime')
+        .on('postgres_changes', onInsert('cctp_transfers'), payload => dispatchEvent('cctp_transfers', payload.new, io))
+        .on('postgres_changes', onUpdate('cctp_transfers'), payload => dispatchEvent('cctp_transfers', payload.new, io))
         .subscribe();
 
     const PORT = process.env.PORT || 3000;
